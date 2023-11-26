b8c25dc0bb76c4c294026d3cb9dfa2d2701a15f8
M-R-D-A <muhammad_rafi_duta_29rpl@student.smktelkom-mlg.sch.id>
2023-11-26 13:14:22 +0800

    fix sidebar hape

--- fe-ukt/context/context.jsx
+++ fe-ukt/context/context.jsx
@@ -4,10 +4,11 @@ export const globalState = createContext()
 function context ({ children }) {
     
     const [showSideBar, setShowSideBar] = useState(true)
+    const [closeSideBar, setCloseSideBar] = useState(false)
     const [active, setActive] = useState('senam')
 
     return ( 
-        <globalState.Provider value={{ showSideBar, setShowSideBar, setActive, active }}>
+        <globalState.Provider value={{ showSideBar, setShowSideBar, setActive, closeSideBar, setCloseSideBar, active }}>
             {children}
         </globalState.Provider>
     )
