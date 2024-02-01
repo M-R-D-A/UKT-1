a2926f779af4996d72f5d0784cdf6cc158fa24dc
M-R-D-A <muhammad_rafi_duta_29rpl@student.smktelkom-mlg.sch.id>
2024-02-01 09:29:21 +0800

    tampilan tabel detail penguji sambung

--- fe-ukt/pages/admin/ukt/content/sambung.jsx
+++ fe-ukt/pages/admin/ukt/content/sambung.jsx
@@ -24,10 +24,10 @@ const sambung = (props) => {
             <td key={item.id_senam} className='border-b-2 border-gray w-[40%]'>
                 <div className="flex flex-row justify-center">
                     <div className="font-semibold w-3/4 bg-navy border-2 border-purple rounded-md rounded-br-none rounded-tr-none text-white py-1.5 px-2">
-                    {item.sambung_siswa.nomor_urut} - {item.sambung_siswa.name}
+                        {item.sambung_siswa.nomor_urut} - {item.sambung_siswa.name}
                     </div>
                     <div className="flex items-center justify-center text-bold font-semibold w-[15%] bg-purple rounded-md rounded-l-none text-white py-1.5 px-2">
-                            {item.nilai}
+                        {item.nilai}
                     </div>
                 </div>
             </td>
@@ -44,25 +44,25 @@ const sambung = (props) => {
 
                 {/* table */}
                 <div className='h-full overflow-y-auto'>
-                    <table className='w-full'>
-                        <thead className='sticky top-0 bg-black'>
-                            <tr className='text-white text-xs'>
+                    <table className='w-full border-spacing-2'>
+                        <thead className='sticky top-0 bg-black border border-white'>
+                            <tr className='text-white text-[10px]'>
                                 <th className='py-3 w-[5%]'>NO</th>
-                                <th>NAME</th>
-                                <th>PUKULAN</th>
-                                <th>SIKUTAN</th>
-                                <th>DENGKULAN</th>
-                                <th>TENDANGAN A</th>
-                                <th>TENDANGAN C</th>
-                                <th>TENDANGAN T</th>
-                                <th>TENDANGAN B</th>
-                                <th>SERKEL ATAS</th>
-                                <th>SERKEL BAWAH</th>
-                                <th>POLA LANGKAH</th>
-                                <th>JATUHAN SERANGA</th>
-                                <th>KAIT/PENGKOL</th>
-                                <th>GUNTINGAN</th>
-                                <th>BANTINGAN SAUTAN</th>
+                                <th className='border border-white w-20'>NAME</th>
+                                <th className='border border-white w-16'>PUKULAN</th>
+                                <th className='border border-white w-16'>SIKUTAN</th>
+                                <th className='border border-white w-16'>DENGKULAN</th>
+                                <th className='border border-white w-16'>TENDANGAN A</th>
+                                <th className='border border-white w-16'>TENDANGAN C</th>
+                                <th className='border border-white w-16'>TENDANGAN T</th>
+                                <th className='border border-white w-16'>TENDANGAN B</th>
+                                <th className='border border-white w-16'>SERKEL ATAS</th>
+                                <th className='border border-white w-16'>SERKEL BAWAH</th>
+                                <th className='border border-white w-16'>POLA LANGKAH</th>
+                                <th className='border border-white w-16'>JATUHAN SERANGA</th>
+                                <th className='border border-white w-16'>KAIT/PENGKOL</th>
+                                <th className='border border-white w-16'>GUNTINGAN</th>
+                                <th className='border border-white w-16'>BANTINGAN SAUTAN</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -70,7 +70,70 @@ const sambung = (props) => {
                                 <>
                                     <tr className='text-green text-center' key={item.id_sambung}>
                                         <td className='border-b-2 text-white py-3 border-gray'>{index + 1}</td>
-                                        <td className='border-b-2 text-white border-gray '>{item.penguji_sambung.name}</td>
+                                        <td className='border-b-2 text-white border-gray text-[10px]'>
+                                            <div className="flex flex-col justify-center">
+                                                <div className='h-10 sm:h-5 flex justify-center items-center'>
+                                                    {item.penguji_sambung.name}
+                                                </div>
+                                                <div className='flex flex-col'>{item.detail_sambung.map((item, index) => (
+                                                    <table className='border border-white rounded-md '>
+                                                        <tr className='h-10 sm:h-16'>
+                                                            <div className='py-4'>
+                                                                {item.sambung_siswa.name}
+                                                            </div>
+                                                        </tr>
+
+                                                    </table>
+                                                ))
+                                                }</div>
+                                                {/* <div>{item.detail_sambung[1].sambung_siswa.name}</div> */}
+                                            </div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
+                                        <td className='h-full flex-col'>
+                                            <div className='h-5  bg-transparent'></div>
+                                            <div className='h-16 p-4 border border-white bg-green'></div>
+                                            <div className='h-16 p-4 border border-white bg-purple'></div>
+                                        </td>
                                         {/* <TdComponent items={(item.detail_sambung)} /> */}
                                     </tr>
                                 </>
