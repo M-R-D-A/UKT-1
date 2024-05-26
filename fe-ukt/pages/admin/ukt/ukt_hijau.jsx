6a48c649b1ebf6866ddc5c73db5c8d8b2cb950f1
M-R-D-A <muhammad_rafi_duta_29rpl@student.smktelkom-mlg.sch.id>
2024-05-26 20:07:47 +0800

    fix event ukt hanya ranting masing masing untuk admin ranting

--- fe-ukt/pages/admin/ukt/ukt_hijau.jsx
+++ fe-ukt/pages/admin/ukt/ukt_hijau.jsx
@@ -20,6 +20,7 @@ const ukt_hijau = () => {
     const [showModalDelete, setShowModalDelete] = useState(false)
 
     // state
+    const [dataUser, setDataUser] = useState({})
     const [dataEvent, setDataEvent] = useState([])
     const [dataRanting, setDataRanting] = useState([])
     const [isActive, setIsActive] = useState(false)
@@ -30,7 +31,10 @@ const ukt_hijau = () => {
     const [tipe, setTipe] = useState('')
 
     // function get data ranting
-    const getDataRanting = () => {
+    const getDataRanting = async () => {
+        const user = localStorage.getItem('admin')
+        const dataUser = JSON.parse(user)
+        setDataUser(dataUser)
         const token = localStorage.getItem('token')
 
         axios.get(BASE_URL + `ranting`, { headers: { Authorization: `Bearer ${token}` } })
@@ -72,6 +76,7 @@ const ukt_hijau = () => {
     }
 
     useEffect(() => {
+        
         getDataEvent()
         getDataRanting()
         isLogged()
@@ -110,23 +115,40 @@ const ukt_hijau = () => {
                         <div className="grid grid-cols-4 gap-x-5 gap-y-3">
 
                             {/* card ranting */}
-                            {dataRanting?.map((item, index) => (
-                                <><button 
-                                    onClick={() => goToEventRanting(item.id_ranting)} key={index + 1} 
-                                    className="bg-navy hover:bg-gradient-to-r from-[#16D4FC] to-[#9A4BE9] rounded-md p-0.5">
-
-                                        {/* inner bg */}
-                                        <div className="bg-navy p-5 rounded-md space-y-5">
-
-                                            {/* ranting name */}
-                                            <h1 className='text-green text-lg'>Ranting {item.id_ranting}</h1>
-
-                                            {/* ranting data count and add button */}
-                                            <h1 className='text-white text-3xl font-semibold tracking-wider'>{dataEvent.filter(a => a.id_ranting === `${item.id_ranting}`).length}</h1>
-                                        </div>
-                                    </button>
-                                </>
-                            ))}
+                            {dataUser.id_role === 'admin ranting'
+                                ? <><button 
+                                        onClick={() => goToEventRanting(dataUser.id_ranting)} key={1} 
+                                        className="bg-navy hover:bg-gradient-to-r from-[#16D4FC] to-[#9A4BE9] rounded-md p-0.5">
+    
+                                            {/* inner bg */}
+                                            <div className="bg-navy p-5 rounded-md space-y-5">
+    
+                                                {/* ranting name */}
+                                                <h1 className='text-green text-lg'>Ranting {dataUser.id_ranting}</h1>
+    
+                                                {/* ranting data count and add button */}
+                                                <h1 className='text-white text-3xl font-semibold tracking-wider'>{dataEvent.filter(a => a.id_ranting === `${dataUser.id_ranting}`).length}</h1>
+                                            </div>
+                                        </button>
+                                    </>
+                                : dataRanting?.map((item, index) => (
+                                    <><button 
+                                        onClick={() => goToEventRanting(item.id_ranting)} key={index + 1} 
+                                        className="bg-navy hover:bg-gradient-to-r from-[#16D4FC] to-[#9A4BE9] rounded-md p-0.5">
+    
+                                            {/* inner bg */}
+                                            <div className="bg-navy p-5 rounded-md space-y-5">
+    
+                                                {/* ranting name */}
+                                                <h1 className='text-green text-lg'>Ranting {item.id_ranting}</h1>
+    
+                                                {/* ranting data count and add button */}
+                                                <h1 className='text-white text-3xl font-semibold tracking-wider'>{dataEvent.filter(a => a.id_ranting === `${item.id_ranting}`).length}</h1>
+                                            </div>
+                                        </button>
+                                    </>
+                                ))
+                            }
                         </div>
                     </div>
                     {/* akhir konten utama */}
